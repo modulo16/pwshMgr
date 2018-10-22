7a5754ff2f2dd7b13fe37276f3d4e59b1bdafaa9
Sam Tucker <pwshmgr@gmail.com>
2018-10-22 22:19:51 +0100

    bug

--- angular/src/app/machine/machine.service.ts
+++ angular/src/app/machine/machine.service.ts
@@ -16,31 +16,31 @@ export class MachineService {
     }
   
     getMachineById(machineID): Observable<Machine> {
-      return this.http.get<Machine>('/api/machines' + machineID)
+      return this.http.get<Machine>('/api/machines/' + machineID)
     }
   
     postMachine(machine: Machine): Observable<Machine> {
-      return this.http.post<Machine>('/api/machines', machine);
+      return this.http.post<Machine>('/api/machines/', machine);
     }
   
     updateMachine(machine: Machine): Observable<Machine> {
-      return this.http.put<Machine>('/api/machines' + machine._id, machine);
+      return this.http.put<Machine>('/api/machines/' + machine._id, machine);
     }
   
     deleteMachine(machineID) {
-      return this.http.delete('/api/machines' + machineID);
+      return this.http.delete('/api/machines/' + machineID);
     }
 
     getJobByMachine(machineID): Observable<Job[]> {
-      return this.http.get<Job[]>('/api/machines' + machineID);
+      return this.http.get<Job[]>('/api/machines/jobs/' + machineID);
     }
 
     getMachineDrives(machineID): Observable<DriveDetails> {
-      return this.http.get<DriveDetails>('/api/machines' + machineID + '/drives');
+      return this.http.get<DriveDetails>('/api/machines/' + machineID + '/drives');
     }
 
     getAlertByMachine(machineID): Observable<Alert[]> {
-      return this.http.get<Alert[]>('/api/machines' + machineID)
+      return this.http.get<Alert[]>('/api/machines/alerts/' + machineID)
     }
 
 }
